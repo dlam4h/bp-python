6b3020bc42c318a9e233a7bef8d8514a1ebddd8e
dlam4h <2386586402@qq.com>
2018-03-17 21:16:28 +0800

    fix a bug

--- dlam.py
+++ dlam.py
@@ -3,14 +3,16 @@ import numpy as np
 import datetime
 import os
 import pickle
+import random
+from tqdm import tqdm
 np.random.seed(2018)
 
 def Sigmoid(x):
-    return 1/(1+np.exp(-x))
+    return 1./(1.+np.exp(-x))
 
 
 def Sigmoid_prime(x):
-    return Sigmoid(x)*(1-Sigmoid(x))
+    return Sigmoid(x)*(1.-Sigmoid(x))
 
 
 def tanh(x):
@@ -34,10 +36,12 @@ class Network(object):
     def __init__(self, net, data, activation='sigmoid'):
         self.net = net 
         self.data = data
-        self.lr = 0.01
+        self.lr = 0.1
         self.loss = []
-        self.datalen = len(data)
+        self.train_acc = []
+        self.datalen = len(data[0])
         self.biases = [np.random.randn(1, i) for i in net[1:]]
+
         self.weights = [np.random.randn(i,j) for i,j in zip(net[:-1],net[1:])]
         if activation == 'sigmoid':
             self.activation = Sigmoid
@@ -54,56 +58,67 @@ class Network(object):
     #前向传播
     def forward(self, a):
         self.values = []
-        self.active_values = [a]
-        i = 0
+        self.active_values = []
+        self.active_values.append(a)
         for w,b in zip(self.weights,self.biases):
             a = np.dot(a,w) + b
             self.values.append(a)
             a = self.activation(a)
             self.active_values.append(a)
-            i += 1
         return a
 
     #测试准确率时候的前向传播
     def test_forward(self, a):
         for w,b in zip(self.weights,self.biases):
+            # print("a:{} w:{} b:{}".format(a.shape,np.array(w).shape,np.array(b).shape))
+            # print("np.dot:{} np.dot+b:{} self.activation(np.dot(a,w) + b):{}".format(np.dot(a,w).shape, (np.dot(a,w) + b).shape, self.activation(np.dot(a,w) + b).shape))
+            # # print(np.array(w).shape)
             a = self.activation(np.dot(a,w) + b)
+
         return a
 
     #训练 次数 批处理大小 学习率 是否打乱数据
-    def train(self,epochs,batch_size,lr = 0.01,shuffle = False):
+    def train(self,epochs,batch_size,lr = 0.1,shuffle = False):
         self.lr = lr
         for epoch in range(epochs):
             print('Epoch {}:'.format(epoch))
             a=[i for i in range(self.datalen)]
             if shuffle:
-                np.random.shuffle(a)
+                random.shuffle(a)
             for i in range(0,self.datalen,batch_size):
                 if i+batch_size<self.datalen:
-                    batch_data, label = self.get_batch(a[i:i+batch_size])
+                    # batch_data, label = self.get_batch(a[i:i+batch_size])
+                    batch_data = self.data[0][i:i+batch_size]
+                    label = self.data[1][i:i+batch_size]
                 else:
-                    batch_data, label = self.get_batch(a[i:self.datalen])
+                    # batch_data, label = self.get_batch(a[i:self.datalen])
+                    batch_data = self.data[0][i:self.datalen]
+                    label = self.data[1][i:self.datalen]
                 self.forward(batch_data)
-                self.update_batch(label,len(batch_data))
-            print("Loss: {} || Acc: {}".format(self.loss[-1],self.evaluate(self.data)/self.datalen))
+                self.update_batch(batch_data,label,len(batch_data))
+            self.train_acc.append(self.evaluate(self.data) / self.datalen)
+            print("Loss: {:.8f} || Train_Acc: {:.8f}".format(self.loss[-1],self.train_acc[-1]))
 
     #根据每个batch来进行梯度下降
-    def update_batch(self,label,batch_size):
-        new_weights = self.weights
-        new_biases = self.biases
+    def update_batch(self,batch_data,label,batch_size):
+        new_biases = [np.zeros(b.shape) for b in self.biases]
+        new_weights = [np.zeros(w.shape) for w in self.weights]
+
         self.loss.append(((self.active_values[-1]-label) ** 2).mean() / 2)
         delta = (self.active_values[-1]-label)*self.activation_prime(self.values[-1])
-        new_biases[-1] = np.reshape((np.sum(delta, axis=0) / len(delta)), (1, self.net[-1]))
+        new_biases[-1] = np.reshape((np.sum(delta, axis=0)), (1, self.net[-1]))
         # new_biases[-1]=delta
         new_weights[-1]=np.dot(self.active_values[-2].T,delta)
         for i in range(2,len(self.net)):
-            delta = np.dot(delta,self.weights[-i+1].T)*self.activation_prime(self.values[-i])
-            new_biases[-i]=np.reshape((np.sum(delta, axis=0) / len(delta)), (1, self.net[i-1]))
+            delta = np.dot(delta,self.weights[-i+1].transpose())*self.activation_prime(self.values[-i])
             # new_biases[-i] = delta
-            new_weights[-i] = np.dot(self.active_values[-i-1].T,delta)
-        for i in range(len(new_weights)-1):
+            new_biases[-i]=np.reshape((np.sum(delta, axis=0)), (1, self.net[i-1]))
+            new_weights[-i] = np.dot(self.active_values[-i-1].transpose(),delta)
+
+        for i in range(len(self.weights)):
             self.weights[i] = self.weights[i] - self.lr*new_weights[i]/batch_size
             self.biases[i] = self.biases[i] - self.lr*new_biases[i]/batch_size
+
 
     #获取每个batch数据
     def get_batch(self,numlist):
@@ -112,11 +127,10 @@ class Network(object):
         for i in numlist:
             a = self.data[0][i]
             b = self.data[1][i]
-            # a, b = self.data[i]
             data.append(a)
             label.append(b)
-        data=np.asarray(data)
-        label=np.asarray(label)
+        data=np.array(data)
+        label=np.array(label)
         return data,label
 
     #加载权重
@@ -136,10 +150,12 @@ class Network(object):
         output.close()
 
     #测试并输出准确率
-    def evaluate(self, data, tr=True):
+    def evaluate(self, data, train=True):
         a,b = data
-        if tr:
-            test_results = [(np.argmax(self.test_forward(x)), np.argmax(y)) for (x, y) in zip(a,b)]
+        # print(self.test_forward(a).shape)
+        # print(b.shape)
+        if train:
+            test_results = [(np.argmax(x), np.argmax(y)) for (x, y) in zip(self.test_forward(a),b)]
         else:
             test_results = [(np.argmax(self.test_forward(x)), y) for (x, y) in data]
         return sum(int(x == y) for (x, y) in test_results)
